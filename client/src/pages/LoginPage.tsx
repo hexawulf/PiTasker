70e1bd4a0c2288cebca07f9204350f3b709ab16a
0xWulf <0xwulf@proton.me>
2025-07-06 15:48:05 +0800

    Update LoginPage.tsx

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import { useState, FormEvent } from 'react';
-import { useNavigate, Link } from 'wouter';
+import { useLocation, Link } from 'wouter';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
