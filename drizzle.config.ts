307eb418dcac38cab392169218a3e397eabbcfc5
0xWulf <0xwulf@proton.me>
2025-07-06 16:12:22 +0800

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "drizzle-kit";
+import { pgTable } from "drizzle-orm/pg-core";
+// not from "drizzle-orm/neon-serverless"
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
